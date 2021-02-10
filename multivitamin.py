7767ce9fe002c529025eb341e2bf33cb4612405d
Iryna Loza <irynloza@gmail.com>
2021-02-10 15:51:26 -0800

    Complete multivitamin

--- multivitamin.py
+++ multivitamin.py
@@ -14,8 +14,41 @@ What is the maximum number of flavours that Rick can mix:
 
 def count_multivitamin(juice, capacity):
     """Return maximum number of juice flavours can mix"""
+    sum_sets = {
+        1: juice
+    }
+
+    #Add data to hashmap
+    for i in range(len(juice)):
+        summ = juice[i]
+        y = i+1
+        count = 1
+        while y < len(juice):
+            summ+= juice[y]
+            y+= 1
+            count+= 1
+            if count not in sum_sets:
+                sum_sets[count] = []
+                sum_sets[count].append(summ) 
+            else:  
+                sum_sets[count].append(summ)            
+        count = 0    
+
+    #Sort capacity
+    for i in range(len(capacity)):
+        for y in range(i+1, len(capacity)):
+            if capacity[i] < capacity[y]:
+                capacity[i], capacity[y] = capacity[y], capacity[i]           
+
+    result = 0
+    #Iterate through hasm map
+    for key in sum_sets:
+        for i in range(len(sum_sets[key])):
+            if sum_sets[key][i] <= capacity[i]:
+                if result < key:
+                    result = key
     
-    return
+    return result
 
 
 
